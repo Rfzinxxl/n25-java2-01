35ae5479ad6da115ed8be7bef8c78151c9172302
Raphael Barbosa Filho <epura.z@gmail.com>
2025-08-18 21:47:17 -0300

    aula 12

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula05/Casting02.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula05/Casting02.java
@@ -14,8 +14,7 @@ public class Casting02 {
         int europePop   =   547_000_000;
         int americasPop =   339_000_000;
         int oceanaPop   =    13_000_000;
-        System.out.println("World Population in 1950: 
-              (+africaPop +asiaPop +europePop +americasPop +oceanaPop));
+        System.out.print("World Population in 1950: "  +  (+africaPop +asiaPop +europePop +americasPop +oceanaPop));
         
         //The current population of Asia is an estimated 60% of the world population
         double percentAsia = 0.6;
